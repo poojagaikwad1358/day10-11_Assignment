62d7819163b3ed1a20d44327e7409afd5cdde0b2
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-07 20:48:23 +0530

    Perform operation1- a+b*c

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 echo "Welcome to Sorting Arithmetic Computation Problem"
 
-echo "Enter Three Number"
-read number1
-read number2
-read number3
+echo "Enter Three Number:"
+read a
+read b
+read c
+operation1=$((a+b*c));
+echo $operation1
